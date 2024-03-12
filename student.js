e92f8c22730d984d857032d7dc85b300fb520772
komikowy <115989484+komikowy@users.noreply.github.com>
2024-03-12 17:43:26 +0100

    student.js

--- student.js
+++ student.js
@@ -1,6 +1,17 @@
-// zad1
+// zad5
 
 const fullName = "Jakub Dyba";
 const studentID = 45244;
 
-console.log(`My name is ${fullName}. My student ID is ${studentID}`);
+function getStudentFullName() {
+  return fullName;
+}
+
+function getStudentId() {
+  return studentID;
+}
+
+module.exports = {
+  getStudentFullName,
+  getStudentId,
+};
